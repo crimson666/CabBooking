f3f6f317890777c970a9bf54c0d733102fcaa802
crimson666 <ankurbsws@gmail.com>
2024-09-24 10:43:35 +0530

    naruto stuff

---
+++ Naruto/src/test/java/com/narutoCharacters/Naruto/NarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narutoCharacters.Naruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
